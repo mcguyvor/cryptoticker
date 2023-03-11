9397449e4b469090263663032766d1abdcb794ee
Noppawat Chotiwan <noppawatchotiwan@gmail.com>
2023-03-11 16:52:38 +0700

    implement symbol static object

--- src/components/TickerDetail.tsx
+++ src/components/TickerDetail.tsx
@@ -1,7 +1,8 @@
-import { Card, Spin, Avatar, Space, Row, Col } from "antd";
+import { Card, Spin, Avatar, Space, Row, Col, Typography } from "antd";
 import { TickerResponse, PAIR } from "../types/ticker";
 import styles from "../styles/Symbol.module.scss";
-import { symbols } from "../static/symbolImg";
+import { symbols } from "../static/symbols";
+
 interface TickerDetailProps {
   symbol: PAIR;
   isLoading: boolean;
@@ -23,38 +24,50 @@ const TickerDetail = ({
     </Card>
   ) : (
     data && (
-      <Card style={{ width: "50%", paddingTop: "0", height: "150px" }}>
-        <Row>
-          <Col span={12}>
-            <div className={styles.tickerTitle}>
-              <Space>
-                <Avatar src={symbols[symbol].img} />
-                <p>{symbols[symbol].displayText}</p>
-              </Space>
-            </div>
-            {isLoading ? (
-              <Spin tip='Loading' />
-            ) : (
-              <Meta
-                title={data.lastPrice}
-                description={`Volume: ${data.volume}`}
-              />
-            )}
-          </Col>
-          <Col span={12} className={styles.priceChange}>
-            {!isLoading && (
-              <div
-                className={
-                  Math.sign(Number(data.priceChange)) === -1
-                    ? styles.primaryRed
-                    : styles.primaryGreen
-                }
-              >
-                <b>{`${data.priceChange}%`}</b>
-              </div>
-            )}
-          </Col>
-        </Row>
+      <Card
+        style={{ width: "50%", paddingTop: "0", height: "150px" }}
+        bodyStyle={{ paddingTop: "0" }}
+      >
+        <div className={styles.tickerTitle}>
+          <Space>
+            <Avatar src={symbols[symbol].img} />
+            <p>{symbols[symbol].displayText}</p>
+          </Space>
+        </div>
+        {isLoading ? (
+          <Spin tip='Loading' />
+        ) : (
+          <>
+            <Row>
+              <Col span={12}>
+                <Meta
+                  title={`${data.lastPrice} THB`}
+                  description={
+                    <div>
+                      Volume: {data.volume}
+                      <br />
+                      High Price: {data.highPrice} THB
+                      <br />
+                      Low Price: {data.lowPrice} THB
+                    </div>
+                  }
+                />
+              </Col>
+              <Col span={12}>
+                <div
+                  className={
+                    Math.sign(Number(data.priceChange)) === -1
+                      ? styles.primaryRed
+                      : styles.primaryGreen
+                  }
+                >
+                  <b>{`${data.priceChange}%`}</b>
+                </div>
+                24h change
+              </Col>
+            </Row>
+          </>
+        )}
       </Card>
     )
   );
